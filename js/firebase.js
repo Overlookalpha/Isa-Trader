a9498f64a71c03c8344562470884dfaef8daa3ab
Overlookalpha <isaiasfr26@gmail.com>
2026-08-05 20:48:49 +0100

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,29 @@
+// Firebase App
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.1/firebase-app.js";
+
+// Firebase Auth
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.7.1/firebase-auth.js";
+
+// Firebase Firestore
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBijFka-NicMl5ayI7dBIGQsPDU1MLnGP8",
+  authDomain: "isatrader-d4602.firebaseapp.com",
+  projectId: "isatrader-d4602",
+  storageBucket: "isatrader-d4602.firebasestorage.app",
+  messagingSenderId: "934093480244",
+  appId: "1:934093480244:web:ec22b3c7b7d9f2d81b8561"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
