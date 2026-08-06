5be2da3252052cb4e409dffa7db53d4018aa82a4
Overlookalpha <isaiasfr26@gmail.com>
2026-08-06 09:04:02 +0100

    Create livemarkets.js

---
+++ js/livemarkets.js
@@ -0,0 +1,26 @@
+const markets = [
+
+"EURUSD",
+
+"GBPUSD",
+
+"USDJPY",
+
+"XAUUSD",
+
+"BTCUSDT",
+
+"ETHUSDT"
+
+];
+
+const container = document.getElementById("markets");
+
+markets.forEach(symbol => {
+
+    container.innerHTML += `
+        <div class="card-info">
+            <span>${symbol}</span>
+            <h2 id="${symbol}">Loading...</h2>
+        </div>
+    `;
