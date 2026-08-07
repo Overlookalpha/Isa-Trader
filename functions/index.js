2fc35be46b0aaa98c17750c8187dddf4157fd00b
Overlookalpha <isaiasfr26@gmail.com>
2026-08-07 12:39:48 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
  * Import function triggers from their respective submodules:  
  *
  * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");  
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
